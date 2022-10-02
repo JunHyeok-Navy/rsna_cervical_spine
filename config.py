3d08879b8935c84984e0ff6bf3ce9ca5788b60b0
Park <gylee206@gmail.com>
2022-10-03 02:49:48 +0900

    test2

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ TRAIN_IMAGE_PATH = 'C:\\Users\\user\\Desktop\\deep-learning\\cervical\\rsna-2022
 TEST_IMAGE_PATH = 'C:\\Users\\user\\Desktop\\deep-learning\\cervical\\rsna-2022-cervical-spine-fracture-detection\\test_images\\'
 
 CORES = 16
-IMAGE_SIZE = (384, 384)
+IMAGE_SIZE = (224, 224)
 
 # Hyperparameters
 N_FOLDS = 5
-N_EPOCHS = 5
+N_EPOCHS = 3
 PATIENCE = 3
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 MODEL_NAME = 'efficientnet_b0'
 LEARNING_RATE = 0.001
 FRAC_LOSS_WEIGHT = 2.
